5167d039fed319d4de8d154b96ae175bb7fba01d
Ivan Guerrero <aivangrop95@gmail.com>
2022-08-31 15:41:53 -0500

    Added Sweet Alert when registering an user

--- Clase06Guerrero/simulador.js
+++ Clase06Guerrero/simulador.js
@@ -1,28 +1,5 @@
 const plan = (a,b,c) => a + b + c
 
-
-function premium(){
-    let cuotas = parseInt (prompt("Ingresa cantidad a meses a diferir"))
-    let inscripcion = 0
-    let mantenimientoAnual= 599
-    let contratoAnual= 5988
-
-    let precioFinal = ((plan(inscripcion,mantenimientoAnual,contratoAnual))/cuotas).toFixed(2)
-    if(cuotas === 1){
-        alert("Su pago de contado es" + " " + "$" + precioFinal)
-        console.log("Su pago de contado es" + " " + "$" + precioFinal)
-    } else if (cuotas === 6){
-        alert("Su pago diferido a 6 meses es" + " " + "$" + precioFinal + "/mes")
-        console.log("Su pago diferido a 6 meses es" + " " + "$" + precioFinal + "/mes")
-    } else if (cuotas === 12){
-        alert("Su pago diferido a 12 meses es" + " " + "$" + precioFinal + "/mes")
-        console.log("Su pago diferido a 12 meses es" + " " + "$" + precioFinal + "/mes")
-    } else if (cuotas !== 1 || 6 || 12){
-        alert("Ingresa cantidad de plazos a diferir: 1 Pago de contado, 6 Meses, 12 Meses")
-        console.log("Ingresa cantidad de plazos a diferir<br> 1 Pago de contado <br> 6 Meses <br> 12 Meses")
-    }
-}
-
 const btnBasico = document.getElementById("btn1");
 btnBasico.addEventListener("click", capturar);
 
@@ -42,14 +19,40 @@ function capturar(){
     let contratoAnual= 3588
     let precioFinal = ((plan(inscripcion,mantenimientoAnual,contratoAnual))/cuotas).toFixed(2)
     
+    if (nombreCapturar === "" || 0){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Favor de ingresar tu nombre!'
+          })
+    } else if (parcialidadesCapturar === "-- Selecciona Tus Parcialidades --") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Favor de ingresar el metodo de pago!'
+          })
+    } else {
+        Swal.fire({
+            icon: 'success',
+            title: 'Completado',
+            text: 'Este usuario ha sido registrado!'
+          })
+        nuevoRegistro = new Registro(nombreCapturar,"Básico",parcialidadesCapturar,parseFloat(precioFinal));
+        agregar();
+    }
 
-    nuevoRegistro = new Registro(nombreCapturar,"Básico",parcialidadesCapturar,parseFloat(precioFinal));
-    agregar();
+
 }
+
    let usuarios = [];
+
+
 
    const btnPremium = document.getElementById('btn2');
    btnPremium.addEventListener("click", capturar2);
+
+
+
    function capturar2(){
     function Registro(nombre,plan,parcialidades,total){
         this.nombre=nombre;
@@ -66,9 +69,27 @@ function capturar(){
     let contratoAnual= 5988
     let precioFinal = ((plan(inscripcion,mantenimientoAnual,contratoAnual))/cuotas).toFixed(2)
     
-
-    nuevoRegistro = new Registro(nombreCapturar,"Premium",parcialidadesCapturar,parseFloat(precioFinal));
-    agregar();
+    if (nombreCapturar === "" || 0){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Favor de ingresar tu nombre!'
+          })
+    } else if (parcialidadesCapturar === "-- Selecciona Tus Parcialidades --") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Favor de ingresar el metodo de pago!'
+          })
+    } else {
+        Swal.fire({
+            icon: 'success',
+            title: 'Completado',
+            text: 'Este usuario ha sido registrado!'
+          })
+        nuevoRegistro = new Registro(nombreCapturar,"Premium",parcialidadesCapturar,parseFloat(precioFinal));
+        agregar();
+    }
 }
    function agregar(){
     usuarios.push(nuevoRegistro);
